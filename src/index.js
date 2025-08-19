94803665f57ccf3a2d6fb7e700e5de1c321aad40
ringo-git <ringoy42@gmail.com>
2025-08-19 16:08:30 +0900

    関数のコンポーネント化

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 import ReactDOM from "react-dom/client";
-import Fragment from 'react'
+// import Fragment from 'react'
+// app.jsで定義した関数をインポート
+import { App } from './App';
 
+// app.jsに関数を移したため、こちらのは削除
 // アロー関数で定義
-const App = () => {
+// const App = () => {
 
-    // return以降が複数行になる場合は()で囲む
-    return (
-        // return以降は1つのタグ(divなど)で囲われている必要がある
-        <div>
-            <h1>こんにちは！</h1>
-            <h1>お元気ですか？</h1>
-        </div>
+//     // return以降が複数行になる場合は()で囲む
+//     return (
+//         // return以降は1つのタグ(divなど)で囲われている必要がある
+//         <div>
+//             <h1>こんにちは！</h1>
+//             <h1>お元気ですか？</h1>
+//         </div>
 
-        // またはFragmentおよび空タグで囲むことも可能
-        // 不要なDOMを生成したくない時に有効
-        // <>
-        //     <h1>こんにちは！</h1>
-        //     <h1>お元気ですか？</h1>
-        // </>
+//         // またはFragmentおよび空タグで囲むことも可能
+//         // 不要なDOMを生成したくない時に有効
+//         // <>
+//         //     <h1>こんにちは！</h1>
+//         //     <h1>お元気ですか？</h1>
+//         // </>
 
-        // <Fragment>
-        //     <h1>こんにちは！</h1>
-        //     <h1>お元気ですか？</h1>
-        // </Fragment>
-    );
-}
+//         // <Fragment>
+//         //     <h1>こんにちは！</h1>
+//         //     <h1>お元気ですか？</h1>
+//         // </Fragment>
+//     );
+// }
 
 // JSX記法
 // react18ではrender関数が廃止され、createRootが推奨されている
 // createRoot関数の引数に「index.htmlのrender箇所」を指定し、変数に格納
-// render関数の引数に「render対象」を指定
+// render関数の引数に「render対象」を指定（この時App.jsのApp関数が対象になる）
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

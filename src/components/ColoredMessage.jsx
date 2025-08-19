c30a7744371e7d88d35ce4117f0a18310c7e18a4
ringo-git <ringoy42@gmail.com>
2025-08-19 17:21:13 +0900

    Propsの分割代入

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,17 +1,25 @@
 // App.jsxからcontentStyleの定義とreturnで使う箇所を移し、app.jsで使えるようエクスポート
 // App.jsから渡されたProps(color, message)を引数として受け取る
-export const ColoredMessage = (props) => {
-    console.log(props);
+// 引数の段階で分割代入することもある
+export const ColoredMessage = ({ color, children }) => {
+// export const ColoredMessage = (props) => {
+
+    // Propsを分割代入（destructure）
+    // プロジェクトによって分割代入しない場合もある
+    // const { color, children } = props;
+
+    // console.log(props);
 
     // JSのオブジェクトでハイフンは使えないため、プロパティはキャメルケースで記述
     // 値はダブルクウォーテーションで囲む
     // Propsから値を指定
     const contentStyle = {
-        color: props.color,
-        // color: "blue",
+        // 分割代入を使えば毎回Props.~と記述する必要がなくなる
+        color: color,
+        // color: props.color,
         fontSize: "20px"
     }
 
-    return  <h1 style={contentStyle}>{props.children}</h1>;
-    // return  <h1 style={contentStyle}>{props.message}</h1>;
+    // 分割代入を使えば毎回Props.~と記述する必要がなくなる
+    return  <h1 style={contentStyle}>{children}</h1>;
 }

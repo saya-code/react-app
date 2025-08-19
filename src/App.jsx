814ae29f35edbc39747294a553824c0a698f7fd5
ringo-git <ringoy42@gmail.com>
2025-08-19 16:24:06 +0900

    イベントの実装方法

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // index.jsに定義した関数を移し、index.jsで使えるようエクスポート
 // コンポーネントファイルは基本的にjsx(tsx)形式
 export const App = () => {
+    // alertイベント
+    const onClickButton = () => {
+        alert("ボタンがクリックされました");
+    };
 
     // return以降が複数行になる場合は()で囲む
     return (
@@ -8,7 +12,10 @@ export const App = () => {
         <div>
             <h1>こんにちは！</h1>
             <h1>お元気ですか？</h1>
+            {/* return内は{}で囲むことでJSやコメントの記述が可能 */}
+            { console.log("TEST")}
+            {/* イベントの実行も可能(イベント名等はキャメルケース) */}
+            <button onClick={onClickButton}>ボタン</button>
         </div>
-
     );
 }
